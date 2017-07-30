8105d142309918672c27dca29e74dc95a86a5bc7
NfN Orange <orange@ff4500.red>
2017-07-30 14:24:02 +0000

    updating works in firefox and chrome now

--- InvisibleVoice.js
+++ InvisibleVoice.js
@@ -3,7 +3,7 @@
 var aSiteYouVisit = window.location.href;
 var waitingTime = "480";
 var defaultObj = {
-	site:  "data:text/html:base64,PCFET0NUWVBFIEhUTUwgUFVCTElDICItLy9XM0MvL0RURCBIVE1MIDQuMDEgVHJhbnNpdGlvbmFsLy9FTiIgImh0dHA6Ly93d3cudzMub3JnL1RSL2h0bWw0L2xvb3NlLmR0ZCI+Cgo8aHRtbD4KICAgIDxib2R5PgogICAgICAgICAgICBIZWxsb3cgCiAgICA8L2JvZHk+CjwvaHRtbD4K",
+	site:  "data:text/html;charset=utf-8;base64,PCFET0NUWVBFIEhUTUwgUFVCTElDICItLy9XM0MvL0RURCBIVE1MIDQuMDEgVHJhbnNpdGlvbmFsLy9FTiIgImh0dHA6Ly93d3cudzMub3JnL1RSL2h0bWw0L2xvb3NlLmR0ZCI+Cgo8aHRtbD4KICAgIDxib2R5PgogICAgICAgICAgICBIZWxsb3cgCiAgICA8L2JvZHk+CjwvaHRtbD4K",
 	url: "bbc.co.uk",
 	time: "1500000000"
 };
@@ -19,16 +19,17 @@ function getData() {
 		var newTime = topSiteOfTheWeek.time + waitingTime;
 		if ( newTime < now ){
 			console.log("[ Invisible Voice ]: Update needed, so updating");
-            fetch('https://antisocial.club/invisibletest.json')
+            fetch("https://antisocial.club/invisibletest.json", {mode: 'no-cors'})
                 .then(response => response.json())
                 .then(data => chrome.storage.local.set(data))
-                .catch(error => console.log(" [ Invisible Voice ]: Fetch Error", error.message ));
+                .catch(error => console.log("[ Invisible Voice ]: Fetch Error", error.message ));
 		}
 
 		// Then you check to see if when you visit a site it might be a top site
 		if (aSiteYouVisit.indexOf(topSiteOfTheWeek.url) >= 0){
 		    // Ooooh watch out, that site was sighted, set sites to the replacement!
-			window.location.replace(topSiteOfTheWeek.site);
+			var site = topSiteOfTheWeek.site;
+			window.location.replace(site);
 			console.log("[ Invisible Voice ]: Page Replaced");
 		}
 	});	
